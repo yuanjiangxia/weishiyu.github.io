8b747c4aac9338aa61fbe29df0ff93ea9f6ee5fc
yuanjiangxia <511217468@qq.com>
2020-12-14 15:37:18 +0800

    fix bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 	var dateDom = document.querySelector('.date');
 	setInterval(function() {
 		var date = new Date();
-		var currDate = [date.getFullYears(), date.getMonth() + 1, date.getDate()].join('-')
+		var currDate = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
 			+ [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
 		dateDom.innerText = currDate;
 	}, 1e3);
