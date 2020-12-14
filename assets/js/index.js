daaa9deb57774f1e504090a29b39fe88fa5b1bbb
yuanjiangxia <511217468@qq.com>
2020-12-14 15:39:01 +0800

    use cdn

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
 	setInterval(function() {
 		var date = new Date();
 		var currDate = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
-			+ [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
+			+ ' ' + [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
 		dateDom.innerText = currDate;
 	}, 1e3);
 })();
