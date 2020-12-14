34970c61c7d609e2a22b3b8ffeb5a2b0fc4732b1
yuanjiangxia <511217468@qq.com>
2020-12-14 15:32:22 +0800

    add index page.

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+(function() {
+	var dateDom = document.querySelector('.date');
+	setInterval(function() {
+		var date = new Date();
+		var currDate = [date.getFullYears(), date.getMonth() + 1, date.getDate()].join('-')
+			+ [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
+		dateDom.innerText = currDate;
+	}, 1e3);
+});
