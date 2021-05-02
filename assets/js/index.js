f7a559d927ebe9a375eb32a36f3b90b7ffafa4e4
yuanjiangxia <511217468@qq.com>
2021-05-02 14:47:39 +0800

    首页提交

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var dateDom = document.querySelector('.date');
-	setInterval(function() {
-		var date = new Date();
-		var currDate = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
-			+ ' ' + [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
-		dateDom.innerText = currDate;
-	}, 1e3);
-})();
